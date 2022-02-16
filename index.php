9ed4f5727ecb3fd4385a5b3643071c7f30d33924
Antoneth Boral <antonethboral0914@gmail.com>
2022-02-16 14:53:02 +0800

    added index file

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Github tutorial</title>
+</head>
+<body>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates dignissimos est vel, blanditiis ullam ratione eius nostrum veritatis nemo nisi reprehenderit maiores suscipit aspernatur minima totam provident laboriosam quaerat ut odio eaque fugiat et officiis beatae reiciendis. Aperiam molestiae sequi ea id, consequatur ad at eum alias minus ratione nemo?</p>
+</body>
+</html>
