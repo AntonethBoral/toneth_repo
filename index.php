f88c438220e58153f92d8cf9f03f1b698279f7e1
AntonethBoral <99782901+AntonethBoral@users.noreply.github.com>
2022-02-16 14:56:13 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,6 @@
     <title>Github tutorial</title>
 </head>
 <body>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates dignissimos est vel, blanditiis ullam ratione eius nostrum veritatis nemo nisi reprehenderit maiores suscipit aspernatur minima totam provident laboriosam quaerat ut odio eaque fugiat et officiis beatae reiciendis. Aperiam molestiae sequi ea id, consequatur ad at eum alias minus ratione nemo?</p>
+    <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates dignissimos est vel, blanditiis ullam ratione eius nostrum veritatis nemo nisi reprehenderit maiores suscipit aspernatur minima totam provident laboriosam quaerat ut odio eaque fugiat et officiis beatae reiciendis. Aperiam molestiae sequi ea id, consequatur ad at eum alias minus ratione nemo?</p>
 </body>
 </html>
